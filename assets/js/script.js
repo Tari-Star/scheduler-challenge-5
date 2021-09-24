f0138539a4bfd2df5991902b808d37b0ac188c43
Tari <bota.maral@gmail.com>
2021-09-24 18:54:17 -0400

    local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,17 +13,17 @@ $(document).ready(function() {
     function trackTime() {
         var timeNow = moment().hour();
 
-        //loop over time-blocks
-        $(".time-block").each(function() {
-            var timeBlock = parseInt($(this).attr("id").split("hour")[1]);
+        //loop over time-hours
+        $(".time-hour").each(function() {
+            var timehour = parseInt($(this).attr("id").split("hour")[1]);
 
             //to check time and indicate past, present, future
-            if(timeBlock < timeNow) {
+            if(timehour < timeNow) {
                 $(this).removeClass("future");
                 $(this).removeClass("present");
                 $(this).addClass("past");
             }
-            else if (timeBlock === timeNow) {
+            else if (timehour === timeNow) {
 
                 $(this).removeClass("past");
                 $(this).removeClass("future");
@@ -34,8 +34,18 @@ $(document).ready(function() {
                 $(this).removeClass("past");
                 $(this).addClass("future");
             }
-            
         })
-
     }
+    //get item from local storage
+    $("#hour9 .description").val(localStorage.getItem("hour9"));
+    $("#hour10 .description").val(localStorage.getItem("hour10"));
+    $("#hour11 .description").val(localStorage.getItem("hour11"));
+    $("#hour12 .description").val(localStorage.getItem("hour12"));
+    $("#hour13 .description").val(localStorage.getItem("hour13"));
+    $("#hour14 .description").val(localStorage.getItem("hour14"));
+    $("#hour15 .description").val(localStorage.getItem("hour15"));
+    $("#hour16 .description").val(localStorage.getItem("hour16"));
+    $("#hour17 .description").val(localStorage.getItem("hour17"));
+
+    trackTime();
 })
