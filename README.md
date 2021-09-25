e8c3d7a2473e142a81e05744a8ef6d6c57d7623c
Tari <bota.maral@gmail.com>
2021-09-25 05:53:17 -0400

    update README.md

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# scheduler-challenge-5
+# Scheduler-challenge-5
+
+## Description
+* A simple calendar application that allows a user to save events for each hour of the day. 
+
+* Current day is displayed at the top of the calendar.
+
+* 9 time blocks for standard business hours.
+
+* Each time block is color-coded to indicate whether it is in the past, present or future.
+
+* User can enter an event, click save button and event will be saved in local storage.
+
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,29 +1,30 @@
-var today = moment().format("dddd, MMM Do YYYY");
-$("#currentDay").html(today);
+var todayDate = moment().format("dddd, MMM Do YYYY");
+$("#currentDay").html(todayDate);
 
 $(document).ready(function() {
-    $("saveBtn").on("click",function() {
+    $(".saveBtn").on("click",function() {
         var text = $(this).siblings(".description").val();
         var time = $(this).parent().attr("id");
 
         //save event in local storage
         localStorage.setItem(time, text);
+        
     })
 
     function trackTime() {
         var timeNow = moment().hour();
 
         //loop over time-hours
-        $(".time-hour").each(function() {
-            var timehour = parseInt($(this).attr("id").split("hour")[1]);
+        $(".time-block").each(function() {
+            var timeBlock = parseInt($(this).attr("id").split("hour")[1]);
 
             //to check time and indicate past, present, future
-            if(timehour < timeNow) {
+            if(timeBlock < timeNow) {
                 $(this).removeClass("future");
                 $(this).removeClass("present");
                 $(this).addClass("past");
             }
-            else if (timehour === timeNow) {
+            else if (timeBlock === timeNow) {
 
                 $(this).removeClass("past");
                 $(this).removeClass("future");
